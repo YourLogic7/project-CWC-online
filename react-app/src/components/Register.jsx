0a5590715688ecb96ff82fc22e4b2836220b8804
m.r.izky.nash <m.r.izky.nash@gmail.com>
2025-10-04 06:36:54 +0000

    add

--- react-app/src/components/Register.jsx
+++ react-app/src/components/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_WEB_URL}/register`, formData);
+      const res = await axios.post(`${import.meta.env.VITE_WEB_URL}/api/register`, formData);
       alert(res.data.msg);
     } catch (err) {
       if (err.response) {
